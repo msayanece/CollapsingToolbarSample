34a09fb8195ce2ebe0098d674b3347912a90ba24
Admin <anshu.p@encoders.co.in>
2017-07-27 16:44:46 +0530

    init commit

---
+++ app/src/main/java/com/example/com/collapsingtoolbarsample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.com.collapsingtoolbarsample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
